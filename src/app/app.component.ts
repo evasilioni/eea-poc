355518deb47cdfb6cc8f9a9297fa137f485e61b9
Eva Silioni <evesino@gmail.com>
2018-03-30 15:19:53 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Country } from './country';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Contacts } from './contacts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  countries: Country[];
+  contacts: Contacts = new Contacts();
+  filteredCountries: Country[];
+
+  constructor(private http: HttpClient) {
+    this.getCountries()
+      .subscribe((data: Country[]) => {
+       
+        this.countries = data;
+      
+      });
+  }
+
+  getCountries(): Observable<Country[]> {
+    return this.http.get<Country[]>("./assets/countries.json");
+  }
+
+  searchCountries(event) {
+    console.log(this.countries);
+    this.filteredCountries = this.countries.filter((country: Country) => country.name.toLowerCase().includes(event.query.toLowerCase()));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import {InputTextModule} from 'primeng/inputtext';
+import {CardModule} from 'primeng/card';
+import {AutoCompleteModule} from 'primeng/autocomplete';
 
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    InputTextModule,
+    FormsModule,
+    CardModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
