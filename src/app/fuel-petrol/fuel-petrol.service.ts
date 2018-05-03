a3cf114d351c0d5e03e35e9189807e6ef8ff9233
Eva Silioni <evesino@gmail.com>
2018-05-03 16:51:15 +0300

    WIP : Month fields to input values

--- src/app/fuel-petrol/fuel-petrol.service.ts
+++ src/app/fuel-petrol/fuel-petrol.service.ts
@@ -165,18 +165,18 @@ export class FuelPetrolService {
 
     getSampleFrequencyControls(): BaseControl<string>[] {
         return [
-            // new TextboxControl({key: 'Jan', label: 'January'}),
-            // new TextboxControl({key: 'Feb', label: 'February'}),
-            // new TextboxControl({key: 'Mar', label: 'March'}),
-            // new TextboxControl({key: 'Apr', label: 'April'}),
-            // new TextboxControl({key: 'May', label: 'May'}),
-            // new TextboxControl({key: 'Jun', label: 'June'}),
-            // new TextboxControl({key: 'Jun', label: 'July'}),
-            // new TextboxControl({key: 'Aug', label: 'August'}),
-            // new TextboxControl({key: 'Sep', label: 'September'}),
-            // new TextboxControl({key: 'Oct', label: 'October'}),
-            // new TextboxControl({key: 'Nov', label: 'November'}),
-            // new TextboxControl({key: 'Dec', label: 'December'}),
+            new TextboxControl({key: 'Jan', label: 'January'}),
+            new TextboxControl({key: 'Feb', label: 'February'}),
+            new TextboxControl({key: 'Mar', label: 'March'}),
+            new TextboxControl({key: 'Apr', label: 'April'}),
+            new TextboxControl({key: 'May', label: 'May'}),
+            new TextboxControl({key: 'Jun', label: 'June'}),
+            new TextboxControl({key: 'Jun', label: 'July'}),
+            new TextboxControl({key: 'Aug', label: 'August'}),
+            new TextboxControl({key: 'Sep', label: 'September'}),
+            new TextboxControl({key: 'Oct', label: 'October'}),
+            new TextboxControl({key: 'Nov', label: 'November'}),
+            new TextboxControl({key: 'Dec', label: 'December'}),
             new TextboxControl({key: 'totalMonthValue', label: 'Total Month Samples', disabled: () => true})
         ];
     }
--- src/app/fuel-petrol/sample-frequency/sample-frequency.component.ts
+++ src/app/fuel-petrol/sample-frequency/sample-frequency.component.ts
@@ -12,6 +12,7 @@ import { GroupControl } from '../../dynamic-forms/controls/group-controll';
 export class SampleFrequencyComponent implements OnInit {
 
     groupName: string;
+    monthsArray: string[];
 
     @Input() controls: BaseControl<string>[];
 
@@ -21,12 +22,32 @@ export class SampleFrequencyComponent implements OnInit {
 
     constructor() {
         this.groupName = 'sampleFrequency';
+
+        this.monthsArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     }
 
     ngOnInit() {
+
+        this.monthsArray.forEach(m => {
+            if (this.group.controls.sampleFrequency.controls[m]) {
+                this.group.controls.sampleFrequency.controls[m].valueChanges
+                .subscribe(data => this.onChanges(data));
+            }
+        });
     }
 
 
+    onChanges(monthValue: any) {
+        // tslint:disable-next-line:radix
+        const val =  this.getNumber(monthValue) + this.getNumber(this.group.controls.sampleFrequency.controls['totalMonthValue'].value);
+        this.group.controls.sampleFrequency.controls['totalMonthValue'].setValue(val);
+    }
+
+
+    getNumber(monthValue: any) {
+        // tslint:disable-next-line:radix
+        return isNaN(parseInt(monthValue)) ? 0 : parseInt(monthValue);
+    }
 }
 
 
